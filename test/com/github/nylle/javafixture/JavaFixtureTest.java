a9753b5b354134d344ea7078d7e5f18d2190af0d
jkockrow <jan.kockrow@immobilienscout24.de>
2016-09-13 17:11:59 +0200

    Testing complex model

--- test/com/github/nylle/javafixture/JavaFixtureTest.java
+++ test/com/github/nylle/javafixture/JavaFixtureTest.java
@@ -2,8 +2,16 @@ package com.github.nylle.javafixture;
 
 import com.github.nylle.javafixture.testobjects.AnotherTestDto;
 import com.github.nylle.javafixture.testobjects.TestDto;
+import com.github.nylle.javafixture.testobjects.complex.AccountManager;
 import com.github.nylle.javafixture.testobjects.complex.Contract;
+import com.github.nylle.javafixture.testobjects.complex.ContractCategory;
+import com.github.nylle.javafixture.testobjects.complex.ContractPosition;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+import org.joda.time.Period;
 import org.junit.Test;
+import java.util.Set;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.hamcrest.core.IsNull.notNullValue;
@@ -44,10 +52,20 @@ public class JavaFixtureTest {
   public void canCreateComplexModel() {
     JavaFixture fixture = new JavaFixture();
 
-    Contract result = fixture.build(Contract.class).create();
-
-    assertThat(result, notNullValue());
+    Contract result = fixture.create(Contract.class);
     assertThat(result, instanceOf(Contract.class));
+    assertThat(result.getBaseContractPosition(), instanceOf(ContractPosition.class));
+    assertThat(result.getAccountManager(), instanceOf(AccountManager.class));
+    assertThat(result.getContractPositions(), instanceOf(Set.class));
+    assertThat(result.getContractPositions().size(), greaterThan(0));
+    assertThat(result.getCategory(), instanceOf(ContractCategory.class));
+    assertThat(result.getCreationDate(), instanceOf(LocalDateTime.class));
+
+    ContractPosition firstContractPosition = result.getContractPositions().iterator().next();
+    assertThat(firstContractPosition, instanceOf(ContractPosition.class));
+    assertThat(firstContractPosition.getContract(), instanceOf(Contract.class));
+    assertThat(firstContractPosition.getStartDate(), instanceOf(LocalDate.class));
+    assertThat(firstContractPosition.getRemainingPeriod(), instanceOf(Period.class));
   }
 
 

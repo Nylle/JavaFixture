8e1e7d91fc7e3afa7926e13ddc8bac78df98f5c9
Jan Kockrow <jan.kockrow@idealo.de>
2019-07-22 10:50:21 +0200

    Preparing randomizer

---
+++ src/main/java/com/github/nylle/javafixture/Randomizer.java
@@ -0,0 +1,12 @@
+package com.github.nylle.javafixture;
+
+public class Randomizer {
+    public static <T> T random(Class<T> type) {
+//        final T t = type.newInstance();
+
+//        type.getFields()
+        return null;
+    }
+
+
+}
--- src/main/java/com/github/nylle/javafixture/Reflector.java
+++ src/main/java/com/github/nylle/javafixture/Reflector.java
@@ -31,6 +31,13 @@ public class Reflector<T> {
         }
     }
 
+    public static boolean isPrimitiveOrPrimitiveWrapperOrString(Class<?> type) {
+        return (type.isPrimitive() && type != void.class) ||
+                type == Double.class || type == Float.class || type == Long.class ||
+                type == Integer.class || type == Short.class || type == Character.class ||
+                type == Byte.class || type == Boolean.class || type == String.class;
+    }
+
     private static <T> T getDefaultValue(Class<T> type) {
         return (T) Array.get(Array.newInstance(type, 1), 0);
     }

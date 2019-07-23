1fe2cfb511cc307f90f99890ff323ef4ba253391
Jan Kockrow <jan.kockrow@idealo.de>
2019-07-23 17:08:39 +0200

    Added message-only constructor

--- src/main/java/com/github/nylle/javafixture/RandomizerException.java
+++ src/main/java/com/github/nylle/javafixture/RandomizerException.java
@@ -2,6 +2,10 @@ package com.github.nylle.javafixture;
 
 public class RandomizerException extends RuntimeException {
 
+    public RandomizerException(final String message) {
+        super(message);
+    }
+
     public RandomizerException(final String message, final Throwable cause) {
         super(message, cause);
     }

fea64a19dc2c06c38bd93f07de74827d19273df5
Jan Kockrow <jan.kockrow@idealo.de>
2019-07-26 14:26:24 +0200

    Removed old stuff

--- src/main/java/com/github/nylle/javafixture/Randomizer.java
+++ src/main/java/com/github/nylle/javafixture/Randomizer.java
@@ -1,11 +1,6 @@
 package com.github.nylle.javafixture;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.util.concurrent.ThreadLocalRandom;
-
 public class Randomizer {
-    private static final int MAX_COLLECTION_SIZE = 10;
 
     final SpecimenFactory specimenFactory = new SpecimenFactory(new Context(new Configuration()));
     final ProxyFactory proxyFactory = new ProxyFactory();
@@ -22,21 +17,16 @@ public class Randomizer {
         }
 
         if (Reflector.isCollection(type)) {
-            //TODO: non-generic empty collection or exception?
-            throw new SpecimenException("Unsupported type: " + type);
+            return specimenFactory.build(type).create();
         }
 
-        if (type.isInterface() && !Reflector.isCollection(type) && !Reflector.isMap(type)) {
+        if (type.isInterface()) {
             return proxyFactory.create(type, this);
         }
 
         //TODO: abstract classes: Modifier.isAbstract(type.getModifiers());
 
         return null;
-    }
-
-    int randomLength() {
-        return ThreadLocalRandom.current().nextInt(2, MAX_COLLECTION_SIZE + 1);
     }
 }
 

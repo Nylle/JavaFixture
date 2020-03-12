c74d7746e48dc7aebdcdd978a80d8afa95659643
Jan <Nylle@users.noreply.github.com>
2020-03-13 00:46:44 +0100

    JAVAFIXTURE-25: Add generic support for annotations

--- src/main/java/com/github/nylle/javafixture/extension/JavaFixtureProvider.java
+++ src/main/java/com/github/nylle/javafixture/extension/JavaFixtureProvider.java
@@ -1,15 +1,20 @@
 package com.github.nylle.javafixture.extension;
 
-import static java.util.Arrays.stream;
-
-import java.util.stream.Stream;
-
+import com.github.nylle.javafixture.Configuration;
+import com.github.nylle.javafixture.Context;
+import com.github.nylle.javafixture.ISpecimen;
+import com.github.nylle.javafixture.SpecimenFactory;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.support.AnnotationConsumer;
 
-import com.github.nylle.javafixture.JavaFixture;
+import java.lang.reflect.Type;
+import java.util.stream.Stream;
+
+import static com.github.nylle.javafixture.ReflectionHelper.castToClass;
+import static com.github.nylle.javafixture.ReflectionHelper.isParameterizedType;
+import static java.util.Arrays.stream;
 
 public class JavaFixtureProvider implements ArgumentsProvider, AnnotationConsumer<TestWithFixture> {
     @Override
@@ -17,8 +22,12 @@ public class JavaFixtureProvider implements ArgumentsProvider, AnnotationConsume
 
     @Override
     public Stream<Arguments> provideArguments(ExtensionContext context) {
+        return Stream.of(Arguments.of(context.getTestMethod().stream().flatMap(m -> stream(m.getGenericParameterTypes()).map(t -> createSpecimen(t).create())).toArray()));
+    }
 
-        return Stream.of(Arguments.of(context.getTestMethod().stream().flatMap(m -> stream(m.getParameterTypes()).map(c -> new JavaFixture().create(c))).toArray()));
-
+    private ISpecimen<?> createSpecimen(Type genericType) {
+        return isParameterizedType(genericType)
+                ? new SpecimenFactory(new Context(new Configuration())).build((Class<?>) castToClass(genericType), genericType)
+                : new SpecimenFactory(new Context(new Configuration())).build((Class<?>) castToClass(genericType));
     }
 }

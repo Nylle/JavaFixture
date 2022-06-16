2120a76ba967bc6f530e9c8304c151f6432f4bc8
Andreas Kutschera <andreas.kutschera@web.de>
2022-06-16 09:14:22 +0200

    feat: construct fixture without class as param
    It makes little sense to call a factory method with an object of the same class
    that we want to fixture, therefore creating a (potentially endless) recursion.
    
    Refs: JF-77

---
+++ src/test/java/com/github/nylle/javafixture/testobjects/factorymethod/FactoryMethodWithItselfAsArgument.java
@@ -0,0 +1,20 @@
+package com.github.nylle.javafixture.testobjects.factorymethod;
+
+public class FactoryMethodWithItselfAsArgument {
+
+    private String value;
+
+    private FactoryMethodWithItselfAsArgument(String value) {
+        this.value = value;
+    }
+    public static FactoryMethodWithItselfAsArgument factoryMethod() {
+        return new FactoryMethodWithItselfAsArgument(null);
+    }
+    public static FactoryMethodWithItselfAsArgument factoryMethodWithItselfAsArgument(FactoryMethodWithItselfAsArgument arg) {
+        return new FactoryMethodWithItselfAsArgument("this factory method should have been filtered out!");
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
